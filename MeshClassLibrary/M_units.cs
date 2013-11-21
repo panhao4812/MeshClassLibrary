4ab5712bfe9c2376c22592f794a5b1df556732c0
panhao <4812ph@163.com>
2013-11-21 17:08:35 +0800

    20131121
    Add Mesh Tree

--- MeshClassLibrary/M_units.cs
+++ MeshClassLibrary/M_units.cs
@@ -221,4 +221,162 @@ namespace MeshClassLibrary
 
 
     }
+
+    public class Vertice : BasicVertice
+    {
+        /// static
+        public Vertice(Point3d p):base(p){}
+        public static List<Point3d> DisplayPos(List<Vertice> vs)
+        {
+            List<Point3d> output = new List<Point3d>();
+            vs.ForEach(delegate(Vertice v) { output.Add(v.pos); });
+            return output;
+        }
+        public static List<string> DisplayEnerge(List<Vertice> vs)
+        {
+            List<string> output = new List<string>();
+            vs.ForEach(delegate(Vertice v) { output.Add(v.energe.ToString()); });
+            return output;
+        }
+        public static List<string> DisplayLife(List<Vertice> vs)
+        {
+            List<string> output = new List<string>();
+            vs.ForEach(delegate(Vertice v) { output.Add(v.dead.ToString()); });
+            return output;
+        }
+        public static void CreateCollection(List<Line> x, out List<IndexPair> id, out  List<Vertice> vs)
+        {
+            id = new List<IndexPair>(); vs = new List<Vertice>();
+            id.Add(new IndexPair(0, 1));
+            vs.Add(new Vertice(x[0].From, 1));
+            vs.Add(new Vertice(x[0].To, 0));
+            for (int i = 1; i < x.Count; i++)
+            {
+                bool sign1 = true;
+                bool sign2 = true;
+                int a = 0, b = 0;
+                for (int j = 0; j < vs.Count; j++)
+                {
+                    if (vs[j].equalTo(x[i].From)) { sign1 = false; a = j; }
+                    if (vs[j].equalTo(x[i].To)) { sign2 = false; b = j; }
+                    if (!sign1 && !sign2) { break; }
+                }
+                if (sign1) { vs.Add(new Vertice(x[i].From)); a = vs.Count - 1; }
+                if (sign2) { vs.Add(new Vertice(x[i].To)); b = vs.Count - 1; }
+                vs[a].Add(b); vs[b].Add(a);
+                id.Add(new IndexPair(a, b));
+            }
+        }   
+        /// ////////////////
+        public Vertice(Point3d p, int index):base(p,index) { }
+        public List<Polyline> edges = new List<Polyline>();
+        public bool transferEnerge(double percentage, ref List<Vertice> vs)
+        {
+            bool sign = false;
+            if (!this.dead && this.energe != 0)
+            {
+                this.dead = true;
+                for (int i = 0; i < this.refer.Count; i++)
+                {
+                    if (vs[this.refer[i]].energe == 0)
+                    {
+                        vs[this.refer[i]].energe = this.energe * percentage;
+                        sign = true;
+                    }
+                }
+            }
+            return sign;
+        }
+        public void CrateEdges(List<Vertice> vs)
+        {
+            if (this.refer.Count == 3)
+            {
+                Point3d p1 = vs[this.refer[0]].pos; Vector3d v1 = p1 - this.pos; v1.Unitize();
+                Point3d p2 = vs[this.refer[1]].pos; Vector3d v2 = p2 - this.pos; v2.Unitize();
+                Point3d p3 = vs[this.refer[2]].pos; Vector3d v3 = p3 - this.pos; v3.Unitize();
+                Plane p = new Plane(p1, p2, p3);
+                Vector3d n = p.Normal; n.Unitize();
+                Point3d N1 = this.pos + n * energe;
+                Point3d N2 = this.pos - n * energe;
+                Vector3d v0 = v1 + v2; v0.Unitize(); v0 *= this.energe;
+                Point3d p12 = this.pos + v0;
+                v0 = v2 + v3; v0.Unitize(); v0 *= this.energe;
+                Point3d p23 = this.pos + v0;
+                v0 = v3 + v1; v0.Unitize(); v0 *= this.energe;
+                Point3d p31 = this.pos + v0;
+                Polyline pl1 = new Polyline(); pl1.Add(N1); pl1.Add(p12); pl1.Add(N2); pl1.Add(p31);
+                Polyline pl2 = new Polyline(); pl2.Add(N1); pl2.Add(p23); pl2.Add(N2); pl2.Add(p12);
+                Polyline pl3 = new Polyline(); pl3.Add(N1); pl3.Add(p23); pl3.Add(N2); pl3.Add(p31);
+                edges.Add(pl1); edges.Add(pl2); edges.Add(pl3);
+            }
+        } 
+    }
+    public class BasicVertice
+    {
+            /////////////////////basic
+    public Point3d pos;
+    public bool dead = false;
+    public List<int> refer = new List<int>();
+    public double energe = 0;
+    public BasicVertice(Point3d p)
+    {
+      pos = new Point3d(p);
+    }
+    public BasicVertice(Point3d p, int index)
+    {
+      pos = new Point3d(p);
+      this.refer.Add(index);
+    }
+    public void Add(int i)
+    {
+      this.refer.Add(i);
+    }
+    public bool equalTo(Point3d pt)
+    {
+      if (pos.DistanceTo(pt) < 0.01) { return true; }
+      return false;
+    }
+    /// //////////////////static
+    public static void CreateCollection(List<Line> x, out List<IndexPair> id, out  List<BasicVertice> vs)
+    {
+        id = new List<IndexPair>(); vs = new List<BasicVertice>();
+      id.Add(new IndexPair(0, 1));
+      vs.Add(new BasicVertice(x[0].From, 1));
+      vs.Add(new BasicVertice(x[0].To, 0));
+      for (int i = 1; i < x.Count; i++)
+      {
+        bool sign1 = true;
+        bool sign2 = true;
+        int a = 0, b = 0;
+        for (int j = 0; j < vs.Count; j++)
+        {
+          if (vs[j].equalTo(x[i].From)) { sign1 = false; a = j; }
+          if (vs[j].equalTo(x[i].To)) { sign2 = false; b = j; }
+          if (!sign1 && !sign2) { break; }
+        }
+        if (sign1) { vs.Add(new BasicVertice(x[i].From)); a = vs.Count - 1; }
+        if (sign2) { vs.Add(new BasicVertice(x[i].To)); b = vs.Count - 1; }
+        vs[a].Add(b); vs[b].Add(a);
+        id.Add(new IndexPair(a, b));
+      }
+    }
+    public static List<Point3d> DisplayPos(List<BasicVertice> vs)
+    {
+      List<Point3d> output = new List<Point3d>();
+      vs.ForEach(delegate(BasicVertice v) { output.Add(v.pos); });
+      return output;
+    }
+    public static List<string> DisplayEnerge(List<BasicVertice> vs)
+    {
+      List<string> output = new List<string>();
+      vs.ForEach(delegate(BasicVertice v) { output.Add(v.energe.ToString()); });
+      return output;
+    }
+    public static List<string> DisplayLife(List<BasicVertice> vs)
+    {
+      List<string> output = new List<string>();
+      vs.ForEach(delegate(BasicVertice v) { output.Add(v.dead.ToString()); });
+      return output;
+    }
+    }
 }
---
+++ MeshClassLibrary/MeshTree.cs
@@ -0,0 +1,112 @@
+﻿using Rhino;
+using Rhino.Geometry;
+using Rhino.DocObjects;
+using Rhino.Collections;
+
+using GH_IO;
+using GH_IO.Serialization;
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+
+using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+using System.Data;
+using System.Drawing;
+using System.Reflection;
+using System.Collections;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Rhino.Geometry.Collections;
+
+namespace MeshClassLibrary
+{
+   public class MeshTree
+    {
+        public MeshTree() { }
+        public MeshCreation mc = new MeshCreation();
+        List<IndexPair> id;
+        List<Vertice> vs;
+        /// <summary>
+        /// unstable method, the level is 10
+        /// </summary>
+   
+        public  Mesh ComputeMeshTree(List<Line> x, Point3d y,double firstEnergy,double EnergyDecrease) {
+            Vertice.CreateCollection(x, out this.id, out this.vs);
+            for (int i = 0; i < vs.Count; i++)
+            {
+                if (vs[i].equalTo(y)) { vs[i].energe = firstEnergy; break; }
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                vs.ForEach(delegate(Vertice v) { v.transferEnerge(EnergyDecrease, ref vs); });
+            }
+
+            for (int i = 0; i < vs.Count; i++)
+            {
+                vs[i].CrateEdges(vs);
+                //Print(vs[i].edges.Count.ToString());
+            }
+            ////////////////
+
+            Mesh mesh = new Mesh();
+            for (int i = 0; i < id.Count; i++)
+            {
+                Polyline pl1 = new Polyline(); Polyline pl2 = new Polyline();
+                if (vs[id[i].J].refer.Count == 3)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (vs[id[i].J].refer[j] == id[i].I)
+                        {
+                            pl1 = vs[id[i].J].edges[j]; break;
+                        }
+                    }
+                }
+                if (vs[id[i].I].refer.Count == 3)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (vs[id[i].I].refer[j] == id[i].J)
+                        {
+                            pl2 = vs[id[i].I].edges[j]; break;
+                        }
+                    }
+                }
+                //Print(pl1.Count.ToString());
+                if (pl1.Count == 4 && pl2.Count == 0)
+                {
+                    Plane p = new Plane(vs[id[i].I].pos, vs[vs[id[i].I].refer[0]].pos - vs[id[i].I].pos);
+                    pl2.AddRange(pl1);
+                    pl2.Transform(Transform.PlanarProjection(p));
+
+                }
+                if (pl1.Count == 0 && pl2.Count == 4)
+                {
+                    Plane p = new Plane(vs[id[i].J].pos, vs[vs[id[i].J].refer[0]].pos - vs[id[i].J].pos);
+                    pl1.AddRange(pl2);
+                    pl1.Transform(Transform.PlanarProjection(p));
+
+                }
+                if (pl1.Count == 4 && pl2.Count == 4)
+                {
+
+                    Plane p1 = new Plane(pl1[0], pl1[1], pl1[2]);
+                    Plane p2 = new Plane(pl2[0], pl2[1], pl2[2]);
+                    if (Vector3d.VectorAngle(p1.Normal, p2.Normal) > Math.PI / 2) pl2.Reverse();
+                    mesh.Append(mc.ClosedBridge(pl1, pl2));
+
+                }
+            }
+
+          return mesh;
+
+
+        }      
+    }
+}
