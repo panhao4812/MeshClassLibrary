b6b1a422df0cd002523ecbbf638e2b5ec8549f03
panhao <4812ph@163.com>
2016-12-20 13:04:13 +0800

    20161220

--- MeshClassLibrary/ToTXT.cs
+++ MeshClassLibrary/ToTXT.cs
@@ -60,5 +60,83 @@ namespace MeshClassLibrary
             sw.Close();
             return true;
         }
+        public string WriteScene(int index, string Name, string Texture)
+        {
+            string str = "";
+            str += "Mesh," + index.ToString() + ",wmesh," + Name + "\n";
+            str += "Mesh," + index.ToString() + ",wmeshcollider," + Name + "\n";
+            str += "Mesh," + index.ToString() + ",texture," + Texture + "\n";
+            str += "Mesh," + index.ToString() + ",location,0,0,0" + "\n";
+            str += "Mesh," + index.ToString() + ",scale,1,1,1" + "\n";
+            return str;
+        }
+        public bool Write(Mesh mesh, string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.OpenOrCreate);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(MeshToObj(mesh));
+            //清空缓冲区、关闭流
+            sw.Flush();
+            sw.Close();
+            return true;
+        }
+        public bool Write(List<Mesh> mesh, string folderpath, string Name)
+        {
+            bool hr = false;
+            if (mesh.Count < 1) return hr;
+            for (int i = 0; i < mesh.Count; i++)
+            {
+                string path = folderpath + "\\" + Name + i.ToString() + ".obj";
+                hr = Write(mesh[i], path);
+            }
+            return hr;
+        }
+        public bool Write(List<Mesh> mesh, string folderpath, List<string> Name)
+        {
+            bool hr = false;
+            if (mesh.Count != Name.Count) return hr;
+            for (int i = 0; i < mesh.Count; i++)
+            {
+                string path = folderpath + "\\" + Name[i] + ".obj";
+                hr = Write(mesh[i], path);
+            }
+            return hr;
+        }
+        public string MeshToObj(Mesh mesh)
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            Rhino.Geometry.Collections.MeshVertexList vs = mesh.Vertices;
+            sb.Append("#vs:" + vs.Count.ToString() + "\n");
+            for (int i = 0; i < vs.Count; i++)
+            {
+                sb.Append(string.Format("v {0} {1} {2}\n", vs[i].X, vs[i].Z, vs[i].Y));
+            }
+
+            Rhino.Geometry.Collections.MeshVertexNormalList ns = mesh.Normals;
+            sb.Append("#ns:" + ns.Count.ToString() + "\n");
+            for (int i = 0; i < ns.Count; i++)
+            {
+                sb.Append(string.Format("vn {0} {1} {2}\n", ns[i].X, ns[i].Z, ns[i].Y));
+            }
+
+            Rhino.Geometry.Collections.MeshTextureCoordinateList ts = mesh.TextureCoordinates;
+            sb.Append("#ts:" + ts.Count.ToString() + "\n");
+            for (int i = 0; i < ts.Count; i++)
+            {
+                sb.Append(string.Format("vt {0} {1}\n", ts[i].X, ts[i].Y));
+            }
+            Rhino.Geometry.Collections.MeshFaceList fs = mesh.Faces;
+            sb.Append("#fs:" + fs.Count.ToString() + "\n");
+            for (int i = 0; i < fs.Count; i++)
+            {
+                if (fs[i].IsTriangle)
+                {
+                }
+                if (fs[i].IsQuad)
+                {
+                }
+            }
+            return sb.ToString();
+        }
     }
 }
