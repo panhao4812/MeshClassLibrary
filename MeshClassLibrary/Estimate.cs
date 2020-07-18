d9679a0c96f43daae1c41a403811bd2fc95f6703
panhao <29347213@qq.com>
2020-07-18 15:46:52 +0800

    update for zzj

--- MeshClassLibrary/Estimate.cs
+++ MeshClassLibrary/Estimate.cs
@@ -1,5 +1,6 @@
 ﻿using MathNet.Numerics.LinearAlgebra.Double;
 using MathNet.Numerics.LinearAlgebra.Generic;
+using MathNet.Numerics.LinearAlgebra.Double.Factorization;
 using Rhino.Geometry;
 using System;
 using System.Collections.Generic;
@@ -386,6 +387,125 @@ namespace MeshClassLibrary
         public override string ToString()
         {
             return this.x.ToString() + " " + this.y.ToString() + " " + this.z.ToString();
+        }
+    }
+    public class LineSolver
+    {
+        public LineSolver() { }
+        // public String output = "";
+        public static double IterationEndValue = 0.00001;
+        public static string Output_Format = "F6";
+        public double x, y, z, i, j, k, radius_result;
+        public void Estimate(List<MathNetPoint> datas)
+        {
+            double sum_x = 0;
+            double sum_y = 0;
+            double sum_z = 0;
+            foreach (MathNetPoint temp in datas)
+            {
+                sum_x += temp.x;
+                sum_y += temp.y;
+                sum_z += temp.z;
+            }
+            sum_x /= datas.Count;
+            sum_y /= datas.Count;
+            sum_z /= datas.Count;
+
+            DenseMatrix jacobian = new DenseMatrix(datas.Count, 3);
+            foreach (MathNetPoint temp in datas)
+            {
+                Vector<double> gradient = new DenseVector(3);
+                gradient[0] = temp.x - sum_x;
+                gradient[1] = temp.y - sum_y;
+                gradient[2] = temp.z - sum_z;
+                jacobian.SetRow(datas.IndexOf(temp), gradient);
+            }
+            Svd svd = jacobian.Svd(true);
+            // get matrix of left singular vectors with first n columns of U
+            Matrix<double> U1 = svd.U().SubMatrix(0, datas.Count, 0, 3);
+            // get matrix of singular values
+            Matrix<double> S = new DiagonalMatrix(3, 3, svd.S().ToArray());
+            // get matrix of right singular vectors
+            Matrix<double> V = svd.VT().Transpose();
+
+            Vector<double> parameters = new DenseVector(3);
+            parameters = V.Column(0);
+            x = sum_x;
+            y = sum_y;
+            z = sum_z;
+            i = parameters[0];
+            j = parameters[1];
+            k = parameters[2];
+        }
+        public override string ToString()
+        {
+            string temp = "x:  " + x.ToString(Output_Format) + "\r\n" +
+                "y:  " + y.ToString(Output_Format) + "\r\n" +
+                "z:  " + z.ToString(Output_Format) + "\r\n" +
+                "a:  " + i.ToString(Output_Format) + "\r\n" +
+                "b:  " + j.ToString(Output_Format) + "\r\n" +
+                "c:  " + k.ToString(Output_Format);
+            return temp;
+        }
+    }
+    public class PlaneSolver
+    {
+        public PlaneSolver() { }
+        // public String output = "";
+        public static double IterationEndValue = 0.00001;
+        public static string Output_Format = "F6";
+        public double x, y, z, i, j, k, radius_result;
+        public void Estimate(List<MathNetPoint> datas)
+        {
+            double sum_x = 0;
+            double sum_y = 0;
+            double sum_z = 0;
+            foreach (MathNetPoint temp in datas)
+            {
+                sum_x += temp.x;
+                sum_y += temp.y;
+                sum_z += temp.z;
+            }
+            sum_x /= datas.Count;
+            sum_y /= datas.Count;
+            sum_z /= datas.Count;
+
+            DenseMatrix jacobian = new DenseMatrix(datas.Count, 3);
+            foreach (MathNetPoint temp in datas)
+            {
+                Vector<double> gradient = new DenseVector(3);
+                gradient[0] = temp.x - sum_x;
+                gradient[1] = temp.y - sum_y;
+                gradient[2] = temp.z - sum_z;
+                jacobian.SetRow(datas.IndexOf(temp), gradient);
+            }
+            MathNet.Numerics.LinearAlgebra.Double.Factorization.Svd
+                svd = jacobian.Svd(true);
+            // get matrix of left singular vectors with first n columns of U
+            Matrix<double> U1 = svd.U().SubMatrix(0, datas.Count, 0, 3);
+            // get matrix of singular values
+            Matrix<double> S = new DiagonalMatrix(3, 3, svd.S().ToArray());
+            // get matrix of right singular vectors
+            Matrix<double> V = svd.VT().Transpose();
+
+            Vector<double> parameters = new DenseVector(3);
+            parameters = V.Column(2);
+            x = sum_x;
+            y = sum_y;
+            z = sum_z;
+            i = parameters[0];
+            j = parameters[1];
+            k = parameters[2];
+        }
+        public override string ToString()
+        {
+            string temp = "x:  " + x.ToString(Output_Format) + "\r\n" +
+                "y:  " + y.ToString(Output_Format) + "\r\n" +
+                "z:  " + z.ToString(Output_Format) + "\r\n" +
+                "a:  " + i.ToString(Output_Format) + "\r\n" +
+                "b:  " + j.ToString(Output_Format) + "\r\n" +
+                "c:  " + k.ToString(Output_Format);
+            return temp;
         }
     }
 }
