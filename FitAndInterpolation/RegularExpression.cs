313df53d46d462757bf41a6a54a0c88e52dbcc99
panhao4812 <29347213@qq.com>
2022-07-26 10:28:42 +0800

    update

--- FitAndInterpolation/RegularExpression.cs
+++ FitAndInterpolation/RegularExpression.cs
@@ -4,11 +4,11 @@ using System.Linq;
 
 namespace FitAndInterpolation
 {
-    public struct IndexPair
+    public struct IndexDomain
     {
         public int I;
         public int J;
-        public IndexPair(int i, int j)
+        public IndexDomain(int i, int j)
         {
             I = i;
             J = j;
@@ -16,12 +16,12 @@ namespace FitAndInterpolation
     }
     public class RegularExpression
     {
-        public string str;
+        private string str;
         private void Print(string text)
         {
             str += text + "\r\n";
         }
-        public void PrintPairList(List<IndexPair> pair)
+        public void PrintPairList(List<IndexDomain> pair)
         {
             for (int i = 0; i < pair.Count; i++)
             {
@@ -35,7 +35,7 @@ namespace FitAndInterpolation
                 Print(i.ToString() + " " + pair[i].ToString() + " ");
             }
         }
-        public List<IndexPair> CombinePair(List<IndexPair> PairList)
+        public List<IndexDomain> CombinePair(List<IndexDomain> PairList)
         {
             List<int> list_int = new List<int>();
             for (int i = 0; i < PairList.Count; i++)
@@ -45,10 +45,10 @@ namespace FitAndInterpolation
             }
             list_int = list_int.Distinct<int>().ToList();
 
-            List<IndexPair> ShortPairs = new List<IndexPair>();
+            List<IndexDomain> ShortPairs = new List<IndexDomain>();
             for (int i = 0; i < list_int.Count - 1; i++)
             {
-                if (list_int[i + 1] == list_int[i] + 1) ShortPairs.Add(new IndexPair(list_int[i], list_int[i + 1]));
+                if (list_int[i + 1] == list_int[i] + 1) ShortPairs.Add(new IndexDomain(list_int[i], list_int[i + 1]));
             }
             PairList.AddRange(ShortPairs);
             List<bool> sign = new List<bool>();
@@ -63,11 +63,11 @@ namespace FitAndInterpolation
             }
             return FromPairArray(list_output);
         }
-        private bool EdgeTest(List<IndexPair> pairs, int input)
+        private bool EdgeTest(List<IndexDomain> pairs, int input)
         {
             double a = input - 0.1; double b = input + 0.1;
             bool signa = false; bool signb = false;
-            foreach (IndexPair pair in pairs)
+            foreach (IndexDomain pair in pairs)
             {
                 double I = (double)pair.I;
                 double J = (double)pair.J;
@@ -77,42 +77,42 @@ namespace FitAndInterpolation
             }
             return (signa && signb);
         }
-        public int CompairPair(IndexPair x, IndexPair y)
+        public int CompairPair(IndexDomain x, IndexDomain y)
         {
             if (x.I < y.I) return -1;
             if (x.I > y.I) return 1;
             return 0;
         }
-        public List<IndexPair> FromIntArray(List<int> input)
+        public List<IndexDomain> FromIntArray(List<int> input)
         {
-            List<IndexPair> output = new List<IndexPair>();
+            List<IndexDomain> output = new List<IndexDomain>();
             input = input.Distinct<int>().ToList();
             for (int i = 0; i < input.Count - 1; i++)
             {
-                output.Add(new IndexPair(input[i], input[i + 1]));
+                output.Add(new IndexDomain(input[i], input[i + 1]));
             }
             return output;
         }
-        public List<IndexPair> FromPairArray(int[] input)
+        public List<IndexDomain> FromPairArray(int[] input)
         {
-            List<IndexPair> output = new List<IndexPair>();
+            List<IndexDomain> output = new List<IndexDomain>();
             for (int i = 0; i < input.Length / 2.0; i++)
             {
                 int a1 = input[i * 2], a2 = input[i * 2 + 1];
                 if (a1 > a2)
                 {
-                    output.Add(new IndexPair(a2, a1));
+                    output.Add(new IndexDomain(a2, a1));
                 }
                 else if (a2 > a1)
                 {
-                    output.Add(new IndexPair(a1, a2));
+                    output.Add(new IndexDomain(a1, a2));
                 }
             }
             return output;
         }
-        public List<IndexPair> FromPIArray(int[] input, int index)
+        public List<IndexDomain> FromPIArray(int[] input, int index)
         {
-            List<IndexPair> output = new List<IndexPair>();
+            List<IndexDomain> output = new List<IndexDomain>();
             for (int i = 0; i < input.Length / 2.0; i++)
             {
                 int a1 = input[i * 2], a2 = input[i * 2 + 1];
@@ -120,28 +120,28 @@ namespace FitAndInterpolation
                 {
                     if (a1 < 359)
                     {
-                        output.Add(new IndexPair(a1 + index * 360, 359 + index * 360));
+                        output.Add(new IndexDomain(a1 + index * 360, 359 + index * 360));
                     }
                     if (a2 > 0)
                     {
-                        output.Add(new IndexPair(0 + index * 360, a2 + index * 360));
+                        output.Add(new IndexDomain(0 + index * 360, a2 + index * 360));
                     }
                 }
                 else if (a2 > a1)
                 {
-                    output.Add(new IndexPair(a1 + index * 360, a2 + index * 360));
+                    output.Add(new IndexDomain(a1 + index * 360, a2 + index * 360));
                 }
             }
             return output;
         }
-        public List<IndexPair> FromPairArray(List<int> input)
+        public List<IndexDomain> FromPairArray(List<int> input)
         {
             return FromPairArray(input.ToArray());
         }
         //必须是I<J
-        public string ExpressionPair(IndexPair pair)
+        public string ExpressionPair(IndexDomain pair)
         {
-            List<IndexPair> pairs = SplitPair(pair);
+            List<IndexDomain> pairs = SplitPair(pair);
             //string str = "^(";
             string str = "";
             for (int i = 0; i < pairs.Count; i++)
@@ -152,7 +152,7 @@ namespace FitAndInterpolation
             //str += ")$";
             return str;
         }
-        public string ExpressionShortPair(IndexPair pair)
+        public string ExpressionShortPair(IndexDomain pair)
         {
             char[] Char_a = pair.I.ToString().ToCharArray();
             char[] Char_b = pair.J.ToString().ToCharArray();
@@ -172,19 +172,13 @@ namespace FitAndInterpolation
             }
             return output;
         }
-        public List<IndexPair> SplitPair(IndexPair pair)
+        public List<IndexDomain> SplitPair(IndexDomain pair)
         {
-            List<IndexPair> output = new List<IndexPair>();
+            List<IndexDomain> output = new List<IndexDomain>();
             char[] Char_a = pair.I.ToString().ToCharArray();
             char[] Char_b = pair.J.ToString().ToCharArray();
             List<int> up_list = new List<int>();
             up_list.Add(pair.I);
-            /*
-            if (Char_a[Char_a.Length - 1] == '9')
-            {
-                Char_a = (pair.I + 1).ToString().ToCharArray();
-            }
-            */
             for (int i = 0; i < Char_b.Length; i++)
             {
                 if (i == 0 && Char_a[Char_a.Length - 1] == '0') continue;
@@ -208,14 +202,13 @@ namespace FitAndInterpolation
             up_list2 = up_list2.Distinct().ToList();
             for (int i = 0; i < up_list2.Count - 1; i++)
             {
-                if (i == 0) { output.Add(new IndexPair(up_list2[i], up_list2[i + 1])); }
+                if (i == 0) { output.Add(new IndexDomain(up_list2[i], up_list2[i + 1])); }
                 else
                 {
-                    output.Add(new IndexPair(up_list2[i] + 1, up_list2[i + 1]));
+                    output.Add(new IndexDomain(up_list2[i] + 1, up_list2[i + 1]));
                 }
             }
-            //  if (output.Count == 1) return output;
-            IndexPair lastpair = output[output.Count - 1];
+            IndexDomain lastpair = output[output.Count - 1];
             output.RemoveAt(output.Count - 1);
             up_list.Clear();
             up_list.Add(pair.J);
@@ -242,15 +235,15 @@ namespace FitAndInterpolation
                 }
             }
             up_list2.Reverse();
-            if (up_list2.Count == 1) { output.Add(new IndexPair(lastpair.I, up_list2[0])); return output; }
-            if (up_list2[0] > lastpair.I) output.Add(new IndexPair(lastpair.I, up_list2[0] - 1));
+            if (up_list2.Count == 1) { output.Add(new IndexDomain(lastpair.I, up_list2[0])); return output; }
+            if (up_list2[0] > lastpair.I) output.Add(new IndexDomain(lastpair.I, up_list2[0] - 1));
 
             for (int i = 0; i < up_list2.Count - 1; i++)
             {
-                if (i == up_list2.Count - 2) { output.Add(new IndexPair(up_list2[i], up_list2[i + 1])); }
+                if (i == up_list2.Count - 2) { output.Add(new IndexDomain(up_list2[i], up_list2[i + 1])); }
                 else
                 {
-                    output.Add(new IndexPair(up_list2[i], up_list2[i + 1] - 1));
+                    output.Add(new IndexDomain(up_list2[i], up_list2[i + 1] - 1));
                 }
             }
             return output;
@@ -281,8 +274,8 @@ namespace FitAndInterpolation
         {
             RegularExpression RE = new RegularExpression();
             List<string> output = new List<string>();
-            List<IndexPair> pa = new List<IndexPair>();
-            List<IndexPair> pb = new List<IndexPair>();
+            List<IndexDomain> pa = new List<IndexDomain>();
+            List<IndexDomain> pb = new List<IndexDomain>();
             int a = Filt(_a);
             int b = Filt(_b);
             List<int> list_int = new List<int>();
@@ -291,8 +284,8 @@ namespace FitAndInterpolation
 
             list_int = list_int.Distinct<int>().ToList();
             if (list_int.Count != 4) return output;
-            pa = RE.FromPIArray(new int[] { a, b - 1 }, _index);
-            pb = RE.FromPIArray(new int[] { b, a - 1 }, _index);
+            pa = RE.FromPIArray(new int[] { a, Filt(b - 1) }, _index);
+            pb = RE.FromPIArray(new int[] { b, Filt(a - 1) }, _index);
             string str = "";
             for (int i = 0; i < pa.Count; i++)
             {
@@ -304,69 +297,6 @@ namespace FitAndInterpolation
             {
                 if (i > 0) str += "|";
                 str += RE.ExpressionPair(pb[i]);
-            }
-            output.Add(str); str = "";
-            return output;
-        }
-    }
-    public class PI3
-    {
-        public int _a, _b,_c, _index;
-        public override string ToString()
-        {
-            string str = _a.ToString() + " ";
-            str += _b.ToString()+" " +_c.ToString();
-            return str;
-        }
-        public int Filt(int a)
-        {
-            a = a % 360;
-            if (a < 0) a = a + 360;
-            return a;
-        }
-        public PI3(int a, int b, int c, int index)
-        {
-            _a = Filt(a);
-            _b = Filt(b);
-            _c = Filt(c);
-            _index = index;
-        }
-        public List<string> ToExpression()
-        {
-            RegularExpression RE = new RegularExpression();
-            List<string> output = new List<string>();
-            List<IndexPair> pa = new List<IndexPair>();
-            List<IndexPair> pb = new List<IndexPair>();
-            List<IndexPair> pc = new List<IndexPair>();
-            int a = Filt(_a);
-            int b = Filt(_b);
-            int c = Filt(_c);
-            List<int> list_int = new List<int>();
-            list_int.Add(a); list_int.Add(Filt(b - 1));
-            list_int.Add(b); list_int.Add(Filt(c - 1));
-            list_int.Add(c); list_int.Add(Filt(a - 1));
-            list_int = list_int.Distinct<int>().ToList();
-            if (list_int.Count != 6) return output;
-            pa = RE.FromPIArray(new int[] { a, b - 1 }, _index);
-            pb = RE.FromPIArray(new int[] { b, c - 1 }, _index);
-            pb = RE.FromPIArray(new int[] { c, a - 1 }, _index);
-            string str = "";
-            for (int i = 0; i < pa.Count; i++)
-            {
-                if (i > 0) str += "|";
-                str += RE.ExpressionPair(pa[i]);
-            }
-            output.Add(str); str = "";
-            for (int i = 0; i < pb.Count; i++)
-            {
-                if (i > 0) str += "|";
-                str += RE.ExpressionPair(pb[i]);
-            }
-            output.Add(str); str = "";
-            for (int i = 0; i < pc.Count; i++)
-            {
-                if (i > 0) str += "|";
-                str += RE.ExpressionPair(pc[i]);
             }
             output.Add(str); str = "";
             return output;
@@ -401,10 +331,10 @@ namespace FitAndInterpolation
         {
             RegularExpression RE = new RegularExpression();
             List<string> output = new List<string>();
-            List<IndexPair> pa = new List<IndexPair>();
-            List<IndexPair> pb = new List<IndexPair>();
-            List<IndexPair> pc = new List<IndexPair>();
-            List<IndexPair> pd = new List<IndexPair>();
+            List<IndexDomain> pa = new List<IndexDomain>();
+            List<IndexDomain> pb = new List<IndexDomain>();
+            List<IndexDomain> pc = new List<IndexDomain>();
+            List<IndexDomain> pd = new List<IndexDomain>();
             int a = Filt(_a);
             int b = Filt(_b);
             int c = Filt(_c);
@@ -416,10 +346,10 @@ namespace FitAndInterpolation
             list_int.Add(d); list_int.Add(Filt(a - 1));
             list_int = list_int.Distinct<int>().ToList();
             if (list_int.Count != 8) return output;
-            pa = RE.FromPIArray(new int[] { a, b - 1 }, _index);
-            pb = RE.FromPIArray(new int[] { b, c - 1 }, _index);
-            pc = RE.FromPIArray(new int[] { c, d - 1 }, _index);
-            pd = RE.FromPIArray(new int[] { d, a - 1 }, _index);
+            pa = RE.FromPIArray(new int[] { a, Filt(b - 1) }, _index);
+            pb = RE.FromPIArray(new int[] { b, Filt(c - 1) }, _index);
+            pc = RE.FromPIArray(new int[] { c, Filt(d - 1) }, _index);
+            pd = RE.FromPIArray(new int[] { d, Filt(a - 1) }, _index);
             string str = "";
             for (int i = 0; i < pa.Count; i++)
             {
@@ -443,6 +373,69 @@ namespace FitAndInterpolation
             {
                 if (i > 0) str += "|";
                 str += RE.ExpressionPair(pd[i]);
+            }
+            output.Add(str); str = "";
+            return output;
+        }
+    }
+    public class PI3
+    {
+        public int _a, _b, _c, _index;
+        public override string ToString()
+        {
+            string str = _a.ToString() + " ";
+            str += _b.ToString() + " " + _c.ToString();
+            return str;
+        }
+        public int Filt(int a)
+        {
+            a = a % 360;
+            if (a < 0) a = a + 360;
+            return a;
+        }
+        public PI3(int a, int b, int c, int index)
+        {
+            _a = Filt(a);
+            _b = Filt(b);
+            _c = Filt(c);
+            _index = index;
+        }
+        public List<string> ToExpression()
+        {
+            RegularExpression RE = new RegularExpression();
+            List<string> output = new List<string>();
+            List<IndexDomain> pa = new List<IndexDomain>();
+            List<IndexDomain> pb = new List<IndexDomain>();
+            List<IndexDomain> pc = new List<IndexDomain>();
+            int a = Filt(_a);
+            int b = Filt(_b);
+            int c = Filt(_c);
+            List<int> list_int = new List<int>();
+            list_int.Add(a); list_int.Add(Filt(b - 1));
+            list_int.Add(b); list_int.Add(Filt(c - 1));
+            list_int.Add(c); list_int.Add(Filt(a - 1));
+            list_int = list_int.Distinct<int>().ToList();
+            if (list_int.Count != 6) return output;
+            pa = RE.FromPIArray(new int[] { a, Filt(b - 1) }, _index);
+            pb = RE.FromPIArray(new int[] { b, Filt(c - 1) }, _index);
+            pc = RE.FromPIArray(new int[] { c, Filt(a - 1) }, _index);
+            string str = "";
+            for (int i = 0; i < pa.Count; i++)
+            {
+                if (i > 0) str += "|";
+                str += RE.ExpressionPair(pa[i]);
+            }
+            output.Add(str); str = "";
+            for (int i = 0; i < pb.Count; i++)
+            {
+                if (i > 0) str += "|";
+                str += RE.ExpressionPair(pb[i]);
+            }
+            output.Add(str); str = "";
+            for (int i = 0; i < pc.Count; i++)
+            {
+                if (i > 0) str += "|";
+                str += RE.ExpressionPair(pc[i]);
             }
             output.Add(str); str = "";
             return output;
